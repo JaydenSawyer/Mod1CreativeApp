db180328f0a96e144cfcec3810365a34b567f3e1
Jayden Sawyer <jayden.sawyer25@d155.org>
2024-09-05 07:27:11 -0500

    Yo

--- Mod1CreativeApp/ViewController.swift
+++ Mod1CreativeApp/ViewController.swift
@@ -7,13 +7,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var secondTextField: UITextField!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var firstTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        firstTextField.delegate = self
+        secondTextField.delegate = self
+            }
+    
+    @IBAction func addButtonAction(_ sender: UIButton) {
+        calculate(operation: .addition)
     }
-
-
+    
+    @IBAction func timesButtonAction(_ sender: UIButton) {
+        calculate(operation: .multiplication)
+    }
+    
+    @IBAction func minusButtonAction(_ sender: UIButton) {
+        calculate(operation: .subtraction)
+    }
+    
+    @IBAction func divideButtonAction(_ sender: UIButton) {
+        calculate(operation: .division)
+    }
+    
+    private func calculate(operation: Operation) {
+        guard let firstText = firstTextField.text,
+    let secondText = secondTextField.text,
+    let firstNumber = Double(firstText),
+    let secondNumber = Double(secondText) else {
+            label.text = "try another input"
+            return
+        }
+        
+        var result: Double = 0.0
+        
+        switch operation {
+        case.addition:
+            result = firstNumber + secondNumber
+        case.subtraction:
+            result = firstNumber - secondNumber
+        case.multiplication:
+            result = firstNumber * secondNumber
+        case.division:
+            if secondNumber != 0 {
+                result = firstNumber / secondNumber
+            } else {
+                label.text = "Cannot divide by zero"
+                return
+            }
+        }
+        
+        label.text = "\(result)"
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
+enum Operation {
+    case addition
+    case subtraction
+    case multiplication
+    case division
+}
